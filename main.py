99c105554a44797ca493571802c23d83f7f3f521
Brian Hines <brian@projectweekend.net>
2015-02-23 08:26:14 -0600

    Threaded callback were not useful here

--- main.py
+++ main.py
@@ -3,7 +3,8 @@
 import os
 import sys
 import time
-from pi_pin_manager import PinManager
+import RPi.GPIO as GPIO
+
 from pika_pack import Broadcaster
 
 
@@ -14,17 +15,28 @@ EXCHANGE = 'gpio_broadcast'
 
 DEVICE_KEY = 'stall_monitor'
 
-PIN_CONFIG_FILE = './pins.yml'
+PIN = 18
 
 
-class PinEventHandler(object):
+class StallMonitor(object):
 
-    def __init__(self, gpio):
-        self._gpio = gpio
+    def __init__(self):
+        self._gpio = GPIO
+        self._pin = PIN
         self._broadcaster = Broadcaster(
             rabbit_url=RABBIT_URL,
             exchange=EXCHANGE)
         self._last_pin_reading = None
+        self.setup_gpio()
+
+    def setup_gpio(self):
+        self._gpio.setmode(self._gpio.BCM)
+        self._gpio.setwarnings(False)
+        self._gpio.setup(
+            self._pin,
+            self._gpio.IN,
+            initial=self._gpio.HIGH,
+            pull_up_down=self._gpio.PUD_UP)
 
     @staticmethod
     def _door_message(is_open):
@@ -33,27 +45,32 @@ class PinEventHandler(object):
             'door_open': bool(is_open)
         }
 
-    def send_stall_activity(self, pin_number):
-        pin_reading = self._gpio.input(pin_number)
+    def send_stall_activity(self):
+        pin_reading = self._gpio.input(self._pin)
 
         if self._last_pin_reading is None or self._last_pin_reading != pin_reading:
             self._last_pin_reading = pin_reading
             self._broadcaster.send(DEVICE_KEY, self._door_message(pin_reading))
 
+    def start(self):
+        while True:
+            self._gpio.wait_for_edge(self._pin, self._gpio.BOTH)
+            self.send_stall_activity()
+
+    def stop(self):
+        self._gpio.cleanup()
+
 
 def main():
-    pins = PinManager(
-        config_file=PIN_CONFIG_FILE,
-        event_handlers=PinEventHandler)
+
+    stall_monitor = StallMonitor()
 
     try:
-        # This has to stay running for the GPIO threaded event callbacks
-        while True:
-            time.sleep(0.500)
+        stall_monitor.start()
     except:
         sys.exit(1)
     finally:
-        pins.cleanup()
+        stall_monitor.stop()
 
 
 if __name__ == '__main__':
