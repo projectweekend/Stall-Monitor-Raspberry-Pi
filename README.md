d99535e89b55c5fed691eebe313e347ae80cfd99
Brian Hines <brian@projectweekend.net>
2015-10-30 13:56:10 -0500

    Take 2

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 ## What this does
 
-This watches for a change in pin 18, which is connected to a reed sensor on the bathroom stall door. When a change happens, a message with the current status of the door and a timestamp is sent to RabbitMQ.
+This watches for a change in pin 18, which is connected to a reed sensor on the bathroom stall door. When a change happens, a message with the current status of the door and a timestamp is PiCloud.
--- main.py
+++ main.py
@@ -1,78 +1,52 @@
 #!/usr/bin/env python
 
-import os
 import sys
-import time
 import RPi.GPIO as GPIO
+from picloud_client import PiCloud
 
-from pika_pack import Broadcaster
-
-
-RABBIT_URL = os.getenv('RABBIT_URL', None)
-assert RABBIT_URL
-
-EXCHANGE = 'gpio_broadcast'
-
-DEVICE_KEY = 'stall_monitor'
 
 PIN = 18
+LAST_READING = None
 
 
 class StallMonitor(object):
 
-    def __init__(self):
-        self._gpio = GPIO
-        self._pin = PIN
-        self._broadcaster = Broadcaster(
-            rabbit_url=RABBIT_URL,
-            exchange=EXCHANGE)
-        self._last_pin_reading = None
-        self.setup_gpio()
+    def __init__(self, pin_num, gpio, picloud):
+        self._pin_num = pin_num
+        self._gpio = gpio
+        self._picloud = picloud
+        self._last_reading = None
 
-    def setup_gpio(self):
         self._gpio.setmode(self._gpio.BCM)
         self._gpio.setwarnings(False)
         self._gpio.setup(
-            self._pin,
+            self._pin_num,
             self._gpio.IN,
             initial=self._gpio.HIGH,
             pull_up_down=self._gpio.PUD_UP)
 
-    @staticmethod
-    def _door_message(is_open):
-        return {
-            'timestamp': int(time.time()),
-            'door_open': bool(is_open)
-        }
-
-    def send_stall_activity(self):
-        pin_reading = self._gpio.input(self._pin)
-
-        if self._last_pin_reading is None or self._last_pin_reading != pin_reading:
-            self._last_pin_reading = pin_reading
-            self._broadcaster.send(DEVICE_KEY, self._door_message(pin_reading))
-
-    def start(self):
+    def run(self):
         while True:
-            self._gpio.wait_for_edge(self._pin, self._gpio.BOTH)
-            self.send_stall_activity()
+            self._gpio.wait_for_edge(self._pin_num, self._gpio.BOTH)
+            current_reading = self._gpio.input(self._pin_num)
+            if self._last_reading is None or self._last_reading != current_reading:
+                self._last_reading = current_reading
+                status = 'open' if current_reading else 'closed'
+                self._picloud.publish(event='stall', data=status)
 
     def stop(self):
         self._gpio.cleanup()
-        self._broadcaster.stop()
 
 
 def main():
-
-    stall_monitor = StallMonitor()
-
+    stall_monitor = StallMonitor(pin_num=18, gpio=GPIO, picloud=PiCloud())
     try:
-        stall_monitor.start()
+        stall_monitor.run()
     except:
         sys.exit(1)
     finally:
         stall_monitor.stop()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
