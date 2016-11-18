51d7a5d6d3497c43ab62f452a0b30c9e6cef3e4b
Brian Hines <brian@projectweekend.net>
2016-11-18 09:44:39 -0600

    Some MQTT stuff

--- main.py
+++ main.py
@@ -1,21 +1,40 @@
 #!/usr/bin/env python
 
+import os
+import ssl
 from signal import pause
 from gpiozero import Button
+import paho.mqtt.client as paho
 
 
-def say_hello():
+IOT_CA_ROOT = '/home/pi/certs/root.pem'
+IOT_CERT = '/home/pi/certs/certificate.pem.crt'
+IOT_KEY = '/home/pi/certs/private.pem.key'
+IOT_URL = os.getenv('IOT_URL')
+assert IOT_URL
+
+
+aws_iot = paho.Client()
+aws_iot.tls_set(
+    ca_certs=IOT_CA_ROOT,
+    certfile=IOT_CERT,
+    keyfile=IOT_KEY,
+    tls_version=ssl.PROTOCOL_TLSv1_2)
+aws_iot.connect(IOT_URL, 8883)
+
+
+def send_close():
     print('hello')
 
 
-def say_bye():
+def send_open():
     print('bye')
 
 
 def main():
     button = Button(5, bounce_time=0.1)
-    button.when_pressed = say_hello
-    button.when_released = say_bye
+    button.when_pressed = send_close
+    button.when_released = send_open
     pause()
 
 
