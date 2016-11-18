13d68ddcfd21bbb9a8555375ebafe637ca54894f
Brian Hines <brian@projectweekend.net>
2016-11-18 08:49:27 -0600

    Ok

--- main.py
+++ main.py
@@ -4,16 +4,18 @@ from signal import pause
 from gpiozero import Button
 
 
-def say_yeah():
-    print('yeah')
+def say_hello():
+    print('hello')
+
+
+def say_bye():
+    print('bye')
 
 
 def main():
     button = Button(5, bounce_time=0.3)
-    while True:
-        print('hey')
-        button.wait_for_press()
-        say_yeah()
+    button.when_pressed = say_hello
+    button.when_released = say_bye
 
 
 if __name__ == "__main__":
