46560873089669a900b6f570c26d69357ae9a8fd
Brian Hines <brian@projectweekend.net>
2016-11-18 07:40:21 -0600

    Pause

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python
 
+from signal import pause
 from gpiozero import Button
+
+
+def say_yeah():
+    print('yeah')
 
 
 def main():
     button = Button(5, bounce_time=0.3)
-    while True:
-        button.wait_for_press()
-        print('yeah')
+    button.when_pressed = say_yeah
+    pause()
 
 
 if __name__ == "__main__":
